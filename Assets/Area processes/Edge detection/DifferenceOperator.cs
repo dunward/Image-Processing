362186ee6535a3fee2341e4e33ad48fad636af09
EGLIK <eglik@odin.vision>
2019-10-23 10:12:56 +0900

    study: Area processes#Edge detection

---
+++ Assets/Area processes/Edge detection/DifferenceOperator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AreaProcesses
+{
+    [ExecuteInEditMode]
+    public class DifferenceOperator : MonoBehaviour
+    {
+        public Texture2D source;
+        [Range(0, 1)] public float threshold;
+
+        private void Start()
+        {
+            GetComponent<RawImage>().texture = GetTexture();
+        }
+
+        public Texture2D GetTexture()
+        {
+            Texture2D newSource = new Texture2D(source.width, source.height);
+
+            for (int i = 0; i < source.width; i++)
+            {
+                for (int k = 0; k < source.height; k++)
+                {
+                    float maximum = 0;
+                    for (int mx = -1; mx <= 1; mx++)
+                    {
+                        for (int my = -1; my <= 0; my++)
+                        {
+                            if (mx != 1 && my == 0)
+                                continue;
+
+                            int x = (int)Mathf.Repeat(i + mx, source.width);
+                            int y = (int)Mathf.Repeat(k + my, source.height);
+
+                            maximum = Mathf.Max(maximum, source.GetPixel(i + mx, k + my).grayscale - source.GetPixel(i - mx, k - my).grayscale);
+                        }
+
+                        if (threshold < maximum)
+                        {
+                            newSource.SetPixel(i, k, new Color(maximum, maximum, maximum));
+                        }
+                        else
+                        {
+                            newSource.SetPixel(i, k, Color.black);
+                        }
+                    }
+                }
+            }
+
+            newSource.Apply();
+
+            return newSource;
+        }
+    }
+}
